53b89684ac734ca939317b9e8a41d14ad2c7eea8
fiona <cbjcl0204@163.com>
2017-07-02 18:10:42 +0800

    实现账号密码登录功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,19 +1,69 @@
 // pages/login/login.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {},
+  passwordInput: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
   },
-  onReady:function(){
-    // 页面渲染完成
+  userNameInput: function (e) {
+    this.setData({
+      loginId: e.detail.value
+    })
   },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  confirm: function () {
+    if (this.data.loginId == null) {
+      wx.showModal({
+        title: "信息提示",
+        content: "用户名不得为空，请输入用户名",
+        showCancel: false,
+      })
+    }
+    else if (this.data.password == null) {
+      wx.showModal({
+        title: "信息提示",
+        content: "密码不得为空，请输入密码",
+        showCancel: false,
+      })
+    } else {
+      wx.showToast({
+        title: '加载中',
+        icon: 'loading'
+      });
+      wx.request({
+        url: 'http://127.0.0.1:8080/wtlib-web/login',//上线的话必须是https，没有appId的本地请求貌似不受影响 
+        data: {
+          loginId: this.data.loginId,
+          password: this.data.password,
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+        success: function (res) {
+          wx.hideToast();
+          var code = res.data.code
+          if (code == 10000) {
+            var data = res.data.data;
+            wx.setStorage({"data":data});
+            wx.redirectTo({url:"/pages/home/home"});
+          } else {
+            wx.showModal({
+              title: "登录失败",
+              content: "账号或密码错误，请重新填写",
+              showCancel: false,
+            })
+          }
+        },
+        fail: function () {
+          wx.hideToast();
+          wx.showModal({
+            title: "请求超时",
+            content: "服务器故障，正在维修中",
+            showCancel: false,
+          })
+        }
+      })
+    }
   }
 })
